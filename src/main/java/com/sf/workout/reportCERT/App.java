06893df08f7424aa055d00c102c8f067c7aba5fd
JuanMED <juan.tous@globant.com>
2023-10-04 13:22:08 -0500

    working on

---
+++ src/main/java/com/sf/workout/reportCERT/App.java
@@ -0,0 +1,13 @@
+package com.sf.workout.reportCERT;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
